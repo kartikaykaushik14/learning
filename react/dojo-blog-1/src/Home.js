66e8de6f5a44bc9c6331396e0848971bbf4cecff
Kartikay <kartikayism@gmail.com>
2023-06-13 15:34:28 -0700

    Chane state on button click

--- react/dojo-blog-1/src/Home.js
+++ react/dojo-blog-1/src/Home.js
@@ -1,18 +1,19 @@
-import { useState } from "react";
-
 const Home = () => {
 
-    const[name, setName] = useState('Bob');
-
     const handleClick = () => {
-        setName('Lucy');
+        console.log('My name is Dom');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('My name is ' + name);
     }
 
     return ( 
         <div className="home">
             <h1>HomePage</h1>
-            <p>{ name }</p>
             <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('Bob')}}>Click Again</button>
         </div>
      );
 }
